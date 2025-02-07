637dee3302602d8af72c864dace4c3074843a680
Rhys Arkins <rhys@arkins.net>
2025-02-07 10:29:03 +0100

    chore: add download script

---
+++ bin/download.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Error: No release argument provided."
+  exit 1
+fi
+
+RELEASE=$1
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+# Check if ../tmp/agent-4-github-enterprise-$RELEASE.zip exists
+if [ ! -f ../tmp/agent-4-github-enterprise-$RELEASE.zip ]; then
+  echo "Downloading agent-4-github-enterprise-$RELEASE.zip"
+  mkdir -p ../tmp
+  curl -o ../tmp/agent-4-github-enterprise-$RELEASE.zip https://integrations.mend.io/release/Agent-for-GitHub-Enterprise/agent-4-github-enterprise-$RELEASE.zip
+fi
+
+if [ ! -f ../tmp/agent-4-github-enterprise-$RELEASE.zip ]; then
+    echo "Error: agent-4-github-enterprise-$RELEASE.zip not found."
+    exit 1
+fi
+
+# Unzip agent-4-github-enterprise-$RELEASE.zip if the folder doesn't exist
+if [ ! -d ../tmp/agent-4-github-enterprise-$RELEASE ]; then
+  echo "Unzipping agent-4-github-enterprise-$RELEASE.zip"
+  unzip -o ../tmp/agent-4-github-enterprise-$RELEASE.zip -d ../tmp
+fi
+
+# Check if ../tmp/agent-4-github-enterprise-$RELEASE exists
+if [ ! -d ../tmp/agent-4-github-enterprise-$RELEASE ]; then
+    echo "Error: agent-4-github-enterprise-$RELEASE not found."
+    exit 1
+fi
